7e9420e3b0353debc4146399bc9d2cfc7a2cef2c
zakaria-lagouader <zakaria.lagouader@gmail.com>
2024-06-28 17:22:00 +0100

    added dashboard page layout

--- src/components/dashboard/dashboard-layout.tsx
+++ src/components/dashboard/dashboard-layout.tsx
@@ -1,4 +1,18 @@
 import { PropsWithChildren } from "react";
-export function DashboardLayout({ children }: PropsWithChildren) {
-	return <div>{children}</div>;
+import Sidebar from "@/components/layout/sidebar";
+import Header from "@/components/layout/header";
+
+export default function DashboardLayout({ children }: PropsWithChildren) {
+	return (
+		<div>
+			<Sidebar />
+
+			<div className="lg:pl-72">
+				<Header />
+				<main className="py-10">
+					<div className="px-4 sm:px-6 lg:px-8">{children}</div>
+				</main>
+			</div>
+		</div>
+	);
 }
--- src/routes/dashboard/index.tsx
+++ src/routes/dashboard/index.tsx
@@ -1,5 +1,10 @@
-import { createFileRoute } from '@tanstack/react-router'
+import DashboardLayout from "@/components/dashboard/dashboard-layout";
+import { createFileRoute } from "@tanstack/react-router";
 
-export const Route = createFileRoute('/dashboard/')({
-  component: () => <div>Hello /dashboard/!</div>
-})
+export const Route = createFileRoute("/dashboard/")({
+	component: Dashboard,
+});
+
+function Dashboard() {
+	return <DashboardLayout>hello</DashboardLayout>;
+}
